789ae557ead08cf3ecc9b368338d20a86ea9f624
DJ <dj.leamen@ontariotechu.net>
2025-06-27 22:30:05 -0400

    add screecher files

---
+++ other/c#/screecher/FollowPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private GameObject wayPoint;
+    private Vector3 wayPointPos;
+
+    private float speed = 64.0f;
+    private void Start()
+    {
+        wayPoint = GameObject.Find("AttackTarget");
+    }
+
+    private void Update()
+    {
+        if (GlobalVariables.pageCount >= 7)
+        {
+            gameObject.SetActive(false);
+        }
+
+        wayPointPos = new Vector3(wayPoint.transform.position.x, wayPoint.transform.position.y, wayPoint.transform.position.z);
+        transform.position = Vector3.MoveTowards(transform.position, wayPointPos, speed = Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Kill")
+        {
+            Debug.Log("Ouch");
+
+        }
+
+    }
+}
---
+++ other/c#/screecher/button.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class button : MonoBehaviour
+{
+    public string mySecondScene = "SampleScene";
+
+    private string nextButton = "SKRUBE ATTACK";
+    private string nextScene;
+    private Rect buttonRect;
+    private int width, height;
+
+    private void Start()
+    {
+        width = Screen.width;
+        height = Screen.height;
+        buttonRect = new Rect(width / 8, height / 3, 3 * width / 4, height / 3);
+    }
+
+    private void OnGUI()
+    {
+        GUIStyle buttonStyle = new GUIStyle(GUI.skin.GetStyle("button"));
+        buttonStyle.alignment = TextAnchor.MiddleCenter;
+        buttonStyle.fontSize = 16 * (width / 200);
+
+        if (GUI.Button(buttonRect, nextButton, buttonStyle))
+        {
+            SceneManager.LoadScene(mySecondScene);
+            gameObject.SetActive(false);
+        }
+        
+
+    }
+
+}
+
+   
+
